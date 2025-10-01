a8ba942731e15e7ec025a2684d6fe247d29a9a10
Aburnett2-rrc <aburnett2@Academic.RRC.CA>
2025-10-01 13:36:41 -0500

    feat: add task routes

---
+++ src/api/v1/routes/taskRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "node_modules/@types/express";
+import * as taskController from "../controllers/taskControllers";
+import { validateRequest } from "../middleware/validate";
+import { taskSchemas } from "../validations/taskValidation";
+
+const router: Router = Router();
+
+router.post("/", validateRequest(taskSchemas.create), taskController.createTask);
